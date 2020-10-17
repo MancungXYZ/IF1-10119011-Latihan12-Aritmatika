0d50919638a1dc68b35ac4dc48989bdb2a2ba8ed
Reihan Wiyanda <Reihan Wiyanda@Mancungs>
2020-10-17 13:03:52 +0700

    IF1-10119011-Latihan12-Aritmatika

---
+++ src/if1/pkg10119011/latihan12/aritmatika/IF110119011Latihan12Aritmatika.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package if1.pkg10119011.latihan12.aritmatika;
+
+/**
+ *
+ * @author 
+ *  Reihan Wiyanda
+ */
+public class IF110119011Latihan12Aritmatika {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       int a = 10;
+       int b = 20;
+       int c = 25;
+       int d = 30;
+       System.out.println("A + B = " + (a+b));
+       System.out.println("A - B = " + (a-b));
+       System.out.println("A x B = " + (a*b));
+       System.out.println("B / A = " + (b/a));
+       System.out.println("B % A = " + (b%a));
+       System.out.println("C % A = " + (c%a));
+       System.out.println("A++ = " + (a++));
+       System.out.println("B-- = " + (b--));
+       System.out.println("D++ = " + (d++));
+       System.out.println("++D = " + (++d));
+    }
+    
+}
